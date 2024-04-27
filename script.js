06f89e6725343b25bbf93554ac8dc46ee4c8684f
Renzios <61353378+Renzios@users.noreply.github.com.>
2024-04-28 03:59:00 +0800

    feat: Added restart button

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+const body = document.querySelector("body");
 const choices = document.querySelectorAll(".choice");
 const score = document.querySelector("#score");
 const roundResult = document.querySelector("#round-result");
 const gameResult = document.querySelector("#game-result");
+
+const restart = document.createElement("button");
+restart.setAttribute("id", "restart");
+restart.textContent = "Play again";
+
+restart.addEventListener("click", restartGame);
 
 choices.forEach((choice) => {
     choice.addEventListener("click", () => playRound(choice.id, getComputerChoice()));
@@ -47,8 +54,21 @@ function playRound(playerChoice, computerChoice) {
 
     if (playerScore === 5) {
         gameResult.textContent = "Game Over! You win!";
+        body.appendChild(restart);
     }
     else if (computerScore === 5) {
         gameResult.textContent = "Game Over! You lose!";
+        body.appendChild(restart);
     }
+}
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+
+    score.textContent = `${playerScore} - ${computerScore}`;
+
+    roundResult.textContent = "";
+    gameResult.textContent = "";
+    body.removeChild(restart);
 }
